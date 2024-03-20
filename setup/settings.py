65ea329e4752fd7721386d12f378976e8ddb72e1
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-03-20 14:05:00 -0300

    creating the validation messages

--- setup/settings.py
+++ setup/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
+
 from pathlib import Path, os
 from dotenv import load_dotenv
 
@@ -136,3 +138,10 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
